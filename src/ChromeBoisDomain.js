ed732e8668a5c69f9e563caa4fd4a1f4d31445a5
Arynn Boniface <arynn.boniface@gmail.com>
2021-01-30 12:58:08 -0500

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -5,23 +5,22 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 export default class ChromeBoisDomain extends Component {
   
   handleMouseMove = (event) => {
-    let xCoord = event.clientX 
-    let yCoord = event.clientY
-    
-    drawChromeBoiAtCoords(xCoord, yCoord)
+    let x = event.clientX
+    let y = event.clientY
     /* TODO: This method should capture the `x` and `y` coordinates of the mouse
      * from the event and use them to invoke the `drawChromeBoiAtCoords`
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
+    drawChromeBoiAtCoords(x, y)
   }
-
-  forKeyPress(event) {
-    if (event.key === 'a') {
-      resize(event.type='+')
-    } else if (event.key === 's') {
-      resize(event.type='-')
-    }
+  
+  /* TODO: Create an event handler which, when fired, invokes the provided
+   * `toggleCycling` function with no arguments. Don't forget the click event
+   * listener that should fire it!
+   */
+  handleMouseClick = () => {
+    toggleCycling();
   }
    
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
@@ -30,13 +29,21 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
+
+   handleKeyPress = (event) => {
+     if (event.key === 'a') {
+       resize('+')
+     } else if (event.key === 's') {
+       resize('-')
+     }
+   }
   
   render() {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
-        onClick={toggleCycling}
-        onKeyPress={this.forKeyPress}
+        onClick={this.handleMouseClick}
+        onKeyPress={this.handleKeyPress}
         width='900'
         height='600'
         tabIndex="0">
